3c580c1e9903e3605edfb52f565b4e876c564098
privateasher17 <dev.ashwani17@gmail.com>
2024-12-17 13:08:39 +0530

    Jstl Configuration

--- ToDoApp/src/main/java/com/ctrl/HomeCtrl.java
+++ ToDoApp/src/main/java/com/ctrl/HomeCtrl.java
@@ -1,13 +1,16 @@
 package com.ctrl;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeCtrl {
 	
 	@RequestMapping("/home")
-	public String home() {
+	public String home(Model m) {
+		String str = "Ashwani Kumar Maurya";
+		m.addAttribute("name", str);
 		return "home";
 	}
 
